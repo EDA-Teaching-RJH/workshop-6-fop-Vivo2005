327d25120501e9fdd6229d14430d87d07e43b55e
Vivo2005 <lvm1@kent.ac.uk>
2026-02-10 12:46:43 +0000

    fin task 1

--- part_1.py
+++ part_1.py
@@ -1,1 +1,15 @@
+sample_bay = ["silica","Basalt","Silica","Iron Dust"]
+print (sample_bay[0])
+print (sample_bay[-1])
+print (len(sample_bay))
 
+for i in range(len(sample_bay)):
+    print ("transmitting sampla data for :" , sample_bay [i])
+
+repeat=0
+new_findings=[]
+while repeat < 3:
+    new_rock= input("New rock type: ")
+    new_findings.append(new_rock)
+    repeat=repeat+1
+print (new_findings)
