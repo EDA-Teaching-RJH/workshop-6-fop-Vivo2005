9b3740ea1b5ea20164b1c299906b52cd66c53479
Vivo2005 <lvm1@kent.ac.uk>
2026-02-10 13:40:51 +0000

    the end of the line

--- part_2.py
+++ part_2.py
@@ -1,3 +1,4 @@
+#task2.1
 rover_status= {
 "battery":100,
 "Heater":"off",
@@ -5,3 +6,17 @@ rover_status= {
 }
 
 print (rover_status["battery"])
+
+#task2.2
+rover_status.update({"battery":85})
+rover_status.update({"heater":"on"})
+rover_status.update({"speeed":5})
+print(rover_status)
+
+#task2.3
+mission_log= [
+{"Site": "Crater A", "Radiation": "Low", "Water": False},
+{"Site": "Dune B", "Radiation": "High", "Water": True}
+]
+for i in len(mission_log):
+    print("site", ["site"][i]," has", ["Radiation"][i], "Radiation")
